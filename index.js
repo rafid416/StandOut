ecd58fbf430898775b5d5ae618a3902afc837f7e
Rafid <rfd_2003@hotmail.com>
2019-12-02 12:35:53 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,169 @@
+$('.carousel').carousel({
+    interval: 4000,
+    pause: false,
+})
+
+$("#img1").mouseenter(function () {
+    $("#imgone").attr("src", "images/dress11.jpg");
+    $("#over1 .text").fadeIn("fast");
+    $(this).mouseleave(function () {
+        $("#imgone").attr("src", "images/dress1.jpg");
+        $(".text").fadeOut("fast");
+    }); 
+});
+
+$("#img2").mouseenter(function () {
+    $("#imgtwo").attr("src", "images/dress22.jpg");
+    $("#over2 .text").fadeIn("fast");
+    $(this).mouseleave(function () {
+        $("#imgtwo").attr("src", "images/dress2.jpg");
+        $(".text").fadeOut("fast");
+    }); 
+});
+
+$("#img3").mouseenter(function () {
+    $("#imgthree").attr("src", "images/dress33.jpg");
+    $("#over3 .text").fadeIn("fast");
+    $(this).mouseleave(function () {
+        $("#imgthree").attr("src", "images/dress3.jpg");
+        $(".text").fadeOut("fast");
+    }); 
+});
+
+$("#img4").mouseenter(function () {
+    $("#imgfour").attr("src", "images/dress44.jpg");
+    $("#over4 .text").fadeIn("fast");
+    $(this).mouseleave(function () {
+        $("#imgfour").attr("src", "images/dress4.jpg");
+        $(".text").fadeOut("fast");
+    }); 
+});
+
+$("#img5").mouseenter(function () {
+    $("#imgfive").attr("src", "images/dress55.jpg");
+    $("#over5 .text").fadeIn("fast");
+    $(this).mouseleave(function () {
+        $("#imgfive").attr("src", "images/dress5.jpg");
+        $(".text").fadeOut("fast");
+    }); 
+});
+
+$("#img6").mouseenter(function () {
+    $("#imgsix").attr("src", "images/dress66.jpg");
+    $("#over6 .text").fadeIn("fast");
+    $(this).mouseleave(function () {
+        $("#imgsix").attr("src", "images/dress6.jpg");
+        $(".text").fadeOut("fast");
+    }); 
+});
+//////////////////////////////////////////////////
+
+$("#kimg1").mouseenter(function () {
+    $("#kimgone").attr("src", "images/kids11.jpg");
+    $("#over1 .text").fadeIn("fast");
+    $(this).mouseleave(function () {
+        $("#kimgone").attr("src", "images/kids1.jpg");
+        $(".text").fadeOut("fast");
+    }); 
+});
+
+$("#kimg2").mouseenter(function () {
+    $("#kimgtwo").attr("src", "images/kids22.jpg");
+    $("#over2 .text").fadeIn("fast");
+    $(this).mouseleave(function () {
+        $("#kimgtwo").attr("src", "images/kids2.jpg");
+        $(".text").fadeOut("fast");
+    }); 
+});
+
+$("#kimg3").mouseenter(function () {
+    $("#kimgthree").attr("src", "images/kids33.jpg");
+    $("#over3 .text").fadeIn("fast");
+    $(this).mouseleave(function () {
+        $("#kimgthree").attr("src", "images/kids3.jpg");
+        $(".text").fadeOut("fast");
+    }); 
+});
+
+$("#kimg4").mouseenter(function () {
+    $("#kimgfour").attr("src", "images/kids44.jpg");
+    $("#over4 .text").fadeIn("fast");
+    $(this).mouseleave(function () {
+        $("#kimgfour").attr("src", "images/kids4.jpg");
+        $(".text").fadeOut("fast");
+    }); 
+});
+
+$("#kimg5").mouseenter(function () {
+    $("#kimgfive").attr("src", "images/kids55.jpg");
+    $("#over5 .text").fadeIn("fast");
+    $(this).mouseleave(function () {
+        $("#kimgfive").attr("src", "images/kids5.jpg");
+        $(".text").fadeOut("fast");
+    }); 
+});
+
+$("#kimg6").mouseenter(function () {
+    $("#kimgsix").attr("src", "images/kids66.jpg");
+    $("#over6 .text").fadeIn("fast");
+    $(this).mouseleave(function () {
+        $("#kimgsix").attr("src", "images/kids6.jpg");
+        $(".text").fadeOut("fast");
+    }); 
+});
+
+//////////////////////////////////////////////////
+
+$("#mimg1").mouseenter(function () {
+    $("#mimgone").attr("src", "images/mens11.jpg");
+    $("#over1 .text").fadeIn("fast");
+    $(this).mouseleave(function () {
+        $("#mimgone").attr("src", "images/mens1.jpg");
+        $(".text").fadeOut("fast");
+    }); 
+});
+
+$("#mimg2").mouseenter(function () {
+    $("#mimgtwo").attr("src", "images/mens22.jpg");
+    $("#over2 .text").fadeIn("fast");
+    $(this).mouseleave(function () {
+        $("#mimgtwo").attr("src", "images/mens2.jpg");
+        $(".text").fadeOut("fast");
+    }); 
+});
+
+$("#mimg3").mouseenter(function () {
+    $("#mimgthree").attr("src", "images/mens33.jpg");
+    $("#over3 .text").fadeIn("fast");
+    $(this).mouseleave(function () {
+        $("#mimgthree").attr("src", "images/mens3.jpg");
+        $(".text").fadeOut("fast");
+    }); 
+});
+
+$("#mimg4").mouseenter(function () {
+    $("#mimgfour").attr("src", "images/mens44.jpg");
+    $("#over4 .text").fadeIn("fast");
+    $(this).mouseleave(function () {
+        $("#mimgfour").attr("src", "images/mens4.jpg");
+        $(".text").fadeOut("fast");
+    }); 
+});
+
+$("#mimg5").mouseenter(function () {
+    $("#mimgfive").attr("src", "images/mens55.jpg");
+    $("#over5 .text").fadeIn("fast");
+    $(this).mouseleave(function () {
+        $("#mimgfive").attr("src", "images/mens5.jpg");
+        $(".text").fadeOut("fast");
+    }); 
+});
+
+$("#mimg6").mouseenter(function () {
+    $("#mimgsix").attr("src", "images/mens66.jpg");
+    $("#over6 .text").fadeIn("fast");
+    $(this).mouseleave(function () {
+        $("#mimgsix").attr("src", "images/mens6.jpg");
+        $(".text").fadeOut("fast");
+    }); 
+});
